134216ef2bb8e5ac88feae518552e5e39a27d734
Sergii IVASYSHYN <sivasysh@e1r6p3.unit.ua>
2017-12-28 17:49:35 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sivasysh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/14 14:34:27 by sivasysh          #+#    #+#             */
+/*   Updated: 2017/12/14 14:34:27 by sivasysh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <unistd.h>
+# define BUFF_SIZE 50000
+
+typedef struct			s_fd_item
+{
+	char				*saved;
+	int					fd;
+	struct s_fd_item	*next;
+}						t_fd_item;
+
+int						get_next_line(const int fd, char **line);
+
+#endif
